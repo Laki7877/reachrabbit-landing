b2576b02d80561f3e9da508a97c6655992205bd2
Natt Phenjati <nattphenjati@gmail.com>
2016-09-22 17:48:17 +0700

    First Commit Yeah!

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/* Select Next or Previous Tab - See http://jsfiddle.net/Sherbrow/h6rQ2/3/ with some modifications */
+
+var $allTabs = $('.tabbable li');
+
+$('#prevtab').on('click', function() {
+    var targetTab = $allTabs.filter('.active').prev('li').find('a');
+    if (targetTab.length > 0) {
+      targetTab.click();
+    } else {
+      $allTabs.last().find('a').click();
+    }
+});
+
+$('#nexttab').on('click', function() {
+    var targetTab = $allTabs.filter('.active').next('li').find('a').click();
+    if (targetTab.length > 0) {
+      targetTab.click();
+    } else {
+      $allTabs.first().find('a').click();
+    }
+});
