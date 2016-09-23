ea4bd6a82b1e1feae488e748adb0f6e39a7380b3
Natt Phenjati <nattphenjati@gmail.com>
2016-09-23 18:29:27 +0700

    remove unused code

--- js/main.js
+++ js/main.js
@@ -19,11 +19,3 @@ $('#nexttab').on('click', function() {
       $allTabs.first().find('a').click();
     }
 });
-
-
-/* Show Dropdown over Hover */
-// $('nav li.dropdown').hover(function() {
-//   $(this).find('.dropdown-menu').stop(true, true).show();
-// }, function() {
-//   $(this).find('.dropdown-menu').stop(true, true).fadeOut(500);
-// });
